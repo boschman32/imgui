dc58868a4d37cd8984f17881adee8214695c6251
boschman32 <boschman32@gmail.com>
2019-07-14 20:53:11 +0200

    [Add] Gitignore and premake file.

---
+++ premake5.lua
@@ -0,0 +1,42 @@
+project "ImGUI"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
+	objdir ("obj/" .. OutputDir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+	
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+		
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+		
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
